77cda60c5da0d17fc59647fe487bd3f4243a82e9
shibily <shibilysuneer10@gmail.com>
2025-05-27 07:51:11 +0530

    clear backend bugs

--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import TYPES from "../config/inversify/types";
-import container from "../config/inversify/container";
+// import TYPES from "../config/inversify/types";
+// import container from "../config/inversify/container";
 
 const router = express.Router()
-const adminCtrl = container.get
+
 
 export default router;
